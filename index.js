c45eb33c75f56ee41aab83722837d9d3627ad731
paul.chang <paul.chang@climate.com>
2015-04-28 08:53:46 -0700

    passing along the headers from the original request

--- index.js
+++ index.js
@@ -1,57 +1,70 @@
-var url    = require('url');
-var http   = require('http');
-var fs     = require('fs');
-var path   = require('path');
+var url = require('url');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 var extend = require('extend');
 
+function proxyRequest(localRequest, localResponse, next) {
 
-function proxyRequest (localRequest, localResponse, next) {
+  var parsedUrl = url.parse('http://' + localRequest.url.slice(1));
 
-  var options = url.parse('http://' + localRequest.url.slice(1));
+  // pass along the headers from the original request
+  var options = _.assign(parsedUrl, {headers: localRequest.headers});
 
   http.request(options, function (remoteRequest) {
+
     if (remoteRequest.statusCode === 200) {
+
       localResponse.writeHead(200, {
-          'Content-Type': remoteRequest.headers['content-type']
+        'Content-Type': remoteRequest.headers['content-type']
       });
+
       remoteRequest.pipe(localResponse);
+
     } else {
       localResponse.writeHead(remoteRequest.statusCode);
       localResponse.end();
     }
-  }).on('error', function(e) {
+  }).on('error', function (e) {
     next();
   }).end();
-};
+}
 
-function Proxy (options) {
+function Proxy(options) {
+
   var config = extend({}, {
     route: ''
   }, options);
 
   return function (localRequest, localResponse, next) {
+
     if (typeof config.root === 'string') {
-      config.root = [config.root]
+      config.root = [config.root];
     } else if (!Array.isArray(config.root)) {
       throw new Error('No root specified')
     }
 
-    var pathChecks = []
-    config.root.forEach(function(root, i) {
-      var p = path.resolve(root)+localRequest.url;
+    var pathChecks = [];
 
-      fs.access(p, function(err) {
-        pathChecks.push(err ? false : true)
+    config.root.forEach(function (root, i) {
+      var p = path.resolve(root) + localRequest.url;
+
+      fs.access(p, function (err) {
+        pathChecks.push(err ? false : true);
         if (config.root.length == ++i) {
+
           var pathExists = pathChecks.some(function (p) {
             return p;
           });
+
           if (pathExists) {
             next();
           } else {
+
             if (localRequest.url.slice(0, config.route.length) === config.route) {
               localRequest.url = localRequest.url.slice(config.route.length);
               proxyRequest(localRequest, localResponse, next);
+
             } else {
               return next();
             }
